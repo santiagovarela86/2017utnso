82dd3d16edd10d8abfa65763139222cbb85ee4c4
gmsimois <gmsimois@gmail.com>
2017-07-08 01:30:00 -0300

    Agrego Shells para Prueba Heap y Memoria

---
+++ Shells/Prueba_Heap_Memoria/Kernel.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+clear 
+echo "Ejecutando Proceso Kernel..."
+cd /home/utnso/git/tp-2017-1c-Nene-Malloc/Kernel
+
+./Kernel /home/utnso/git/tp-2017-1c-Nene-Malloc/Configuraciones/Prueba_Heap_Memoria/Kernel
+
+exit 0
