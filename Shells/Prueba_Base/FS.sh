68f064f165b2ec235a777032b0da8f5af2f66772
gmsimois <gmsimois@gmail.com>
2017-07-08 01:07:06 -0300

    Agrego Shells para ejecutar Prueba Base

---
+++ Shells/Prueba_Base/FS.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+clear 
+echo "Ejecutando Proceso FileSystem..."
+cd /home/utnso/git/tp-2017-1c-Nene-Malloc/FileSystem/
+
+./FileSystem /home/utnso/git/tp-2017-1c-Nene-Malloc/Configuraciones/Prueba_Base/FS
+
+exit 0
