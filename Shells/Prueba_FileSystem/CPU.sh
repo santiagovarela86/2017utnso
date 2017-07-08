2e50d080cbf07e624d5e6579890a87bb228241eb
gmsimois <gmsimois@gmail.com>
2017-07-08 01:22:32 -0300

    Agrego shells para Pruebas FileSystem

---
+++ Shells/Prueba_FileSystem/CPU.sh
@@ -0,0 +1,8 @@
+#!/bin/sh
+clear 
+echo "Ejecutando Proceso CPU..."
+cd /home/utnso/git/tp-2017-1c-Nene-Malloc/CPU
+
+./CPU /home/utnso/git/tp-2017-1c-Nene-Malloc/Configuraciones/Prueba_FileSystem/CPU
+
+exit 0
